806c8f46d20f701ba2a3f9302d656153ff5d9787
Akshaya Kumar T <t.akshaya.kumar17@gmail.com>
2019-01-08 21:18:50 +0530

    re-initialized to start

--- performance-appraisal-ui/performance-appraisal/src/app/app.module.ts
+++ performance-appraisal-ui/performance-appraisal/src/app/app.module.ts
@@ -3,15 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { LeaveManagmentComponent } from './leave-managment/leave-managment.component';
-import { LeaveCardSelfComponent } from './leave-card-self/leave-card-self.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    LeaveManagmentComponent,
-    LeaveCardSelfComponent
   ],
   imports: [
     BrowserModule,
